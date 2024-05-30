e3611383b7622c0e361884476ba6025cff66c2a8
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2024-05-30 16:33:24 -0400

    Add devise parameter sanitizer and enabled scoped views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    if resource_class == User
+      devise_parameter_sanitizer.permit(:sign_up, keys: %i[name birthdate])
+    elsif resource_class == Company
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:company_name])
+    end
+  end
 end
